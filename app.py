13b6d0003acfbcc8c7b294cfd9ba56e9a80c7fdc
InariInDream <inariindream@163.com>
2024-04-18 16:22:24 +0800

    fix app.py

--- app.py
+++ app.py
@@ -1,31 +1,33 @@
 from flask import Flask, render_template, request, send_from_directory, abort
 import os
-from file_browser import display_directory_content, get_version_and_movie_data, has_difference
-from imagediff import image_diff
+from file_browser import display_directory_content, get_version_directories, get_movie_frame_versions
+from imagediff import image_diff, has_image_difference
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    dir_path = request.args.get('dir', '.')
-    if not os.path.exists(dir_path) or not os.path.isdir(dir_path):
-        abort(404)
-    files, breadcrumbs = display_directory_content(dir_path)
-    return render_template('index.html', files=files, breadcrumbs=breadcrumbs, current_dir=dir_path)
+    version_dirs = get_version_directories()
+    movies = get_movie_frame_versions(version_dirs)
+    return render_template('index.html', movies=movies, version_dirs=version_dirs)
 
-@app.route('/movie/<string:movie_name>')
+@app.route('/movie_details/<movie_name>')
 def movie_details(movie_name):
-    data = get_version_and_movie_data(movie_name=movie_name)
-    return render_template('movie_details.html', data=data, movie_name=movie_name)
+    version_dirs = get_version_directories()
+    frames = get_movie_frame_versions(version_dirs, movie_name)
+    return render_template('movie_details.html', movie_name=movie_name, frames=frames, version_dirs=version_dirs)
 
-@app.route('/version/<int:version>')
+@app.route('/version_details/<version>')
 def version_details(version):
-    files, has_diffs = has_difference(version)
-    return render_template('version_details.html', files=files, has_diffs=has_diffs, version=version)
+    previous_version = str(int(version) - 1)  # Assuming version naming is sequential integers
+    diffs = has_image_difference(version, previous_version)
+    return render_template('version_details.html', version=version, diffs=diffs)
 
-@app.route('/display_images/<int:version>/<string:movie>/<int:frame_number>')
-def display_images(version, movie, frame_number):
-    image_path = f"{version}/{movie}-{frame_number}.png"
+@app.route('/display_images')
+def display_images():
+    image_path = request.args.get('image', '')
+    if not os.path.isfile(image_path):
+        abort(404)
     diff_results = image_diff(image_path)
     return render_template('display_images.html', diff_results=diff_results, image_path=image_path)
 
